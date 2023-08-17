2ae53dd8f3b957da0a538c10faa4e3ee5e2c7019
HoutarouOreki <thezjarek@gmail.com>
2023-08-17 06:56:51 +0200

    update connectionString, frontend contact service

--- frontend/src/app/contact.service.ts
+++ frontend/src/app/contact.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpEvent, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Contact } from './contact';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  readonly url = 'https://localhost:7298/contacts';
+
+  constructor(private http: HttpClient) { }
+
+  getContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>(this.url);
+  }
+
+  addContact(contact: Contact): Observable<any> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<Contact>(this.url, contact, httpOptions);
+  }
+
+  editContact(contact: Contact): Observable<any> {
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+    return this.http.post<Contact>(this.url + '/edit', contact, httpOptions);
+  }
+
+  deleteContact(id: number): Observable<any> {
+    return this.http.delete<Contact>(this.url + '/' + id);
+  }
 }
