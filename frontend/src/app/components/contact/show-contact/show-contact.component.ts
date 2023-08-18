44317dacc5133ab0480458117d2fa2fbf3aafe1e
HoutarouOreki <thezjarek@gmail.com>
2023-08-18 22:16:56 +0200

    add missing City form field, address formatting

--- frontend/src/app/components/contact/show-contact/show-contact.component.ts
+++ frontend/src/app/components/contact/show-contact/show-contact.component.ts
@@ -10,4 +10,14 @@ export class ShowContactComponent {
   @Input({ required: true }) contact!: Contact
   @Output() onEditButton = new EventEmitter();
   @Output() onRemoveButton = new EventEmitter();
+
+  getFormattedAddress(contact: Contact): string {
+    var cityZipCode = [contact.city, contact.zipCode]
+      .filter(x => x != undefined && x.length > 0)
+      .join(' ');
+    var fullAddress = [contact.address, cityZipCode]
+      .filter(x => x != undefined && x.length > 0)
+      .join(', ');
+    return fullAddress;
+  }
 }
